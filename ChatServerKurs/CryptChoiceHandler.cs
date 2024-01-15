0b9d2215418a5188ec3a15bbcbe965259b7d1506
leahalarsanik@gmail.com <leahalarsanik@gmail.com>
2024-01-15 09:25:56 +0300

    345

--- ChatServerKurs/CryptChoiceHandler.cs
+++ ChatServerKurs/CryptChoiceHandler.cs
@@ -1,13 +1,48 @@
-﻿namespace ChatServerKurs
+﻿using System.Reflection;
+
+namespace ChatServerKurs;
+
+public enum Crypts
 {
-    public enum Crypts {
-        Caesar,
-        PolybiusSquare,
-        Verman,
-        Non
-    }
-    public static class CryptChoiceHandler
+    Caesar,
+    PolybiusSquare,
+    Verman,
+    Non
+}
+
+public static class CryptChoiceHandler
+{
+    private static Crypts crypt;
+
+    public static Crypts ChosenCrypt
     {
-        public static Crypts ChosenCrypt { get; set; }
+        get => crypt;
+        set
+        {
+            crypt = value;
+
+            if(crypt == Crypts.Non)
+                return;
+            try
+            {
+                var temp = Assembly.LoadFrom("Crypter.dll");
+                var t = temp.GetTypes();
+                Encrypt = t[0].GetMethod("Encrypt" + crypt);
+                Decrypt = t[0].GetMethod("Decrypt" + crypt);
+
+              
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("The server was launched without cryption\n");
+                crypt = Crypts.Non;
+                return;
+            }
+        }
     }
+
+    public static MethodInfo Encrypt { get; set; }
+
+    public static MethodInfo Decrypt { get; set; }
 }
